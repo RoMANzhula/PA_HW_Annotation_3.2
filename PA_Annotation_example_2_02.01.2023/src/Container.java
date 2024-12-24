776d8a2e25f7cadfaf09baecd86a9799679559a4
Roman Manzhula <roman1985al@ukr.net>
2024-12-24 21:46:44 +0200

    Update Container.java
    In Ukrainian

--- PA_Annotation_example_2_02.01.2023/src/Container.java
+++ PA_Annotation_example_2_02.01.2023/src/Container.java
@@ -2,29 +2,29 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-@SaveToAnnotation(fileParam1 = "C:\\Users\\Admin\\Desktop\\Новый текстовый документ.txt") //аннотация  параметром - адрес файла
-public class Container { //класс Контейнер для данных - которые мы хотим сохранить
-    private final String text = "If you going to San Francisco..."; //текст, который нужно сохранить
+@SaveToAnnotation(fileParam1 = "C:\\Users\\Admin\\Desktop\\Новый текстовый документ.txt") //анотація з параметром - адреса файлу
+public class Container { // клас Контейнер для даних, які ми хочемо зберегти
+    private final String text = "If you going to San Francisco..."; // текст, який потрібно зберегти
 
-    public String getText() { //геттер для переменной, которая хранит текст для сохранения (можно использовать для проверки
-        //изменения данных в рефлексных методах)
+    public String getText() { // гетер для змінної, яка зберігає текст для збереження (можна використовувати для перевірки
+        // зміни даних в рефлексивних методах)
         return text;
     }
 
-    @SaverAnnotation //аннотация
-    public void save() throws IOException { //метод для сохранения текста в документ
-        var fileForWriter = Container.class.getAnnotation(SaveToAnnotation.class).fileParam1(); //создаем переменную = файл
-        //для сохранения данных (в качестве адреса используем аргумент параметра аннотации)
+    @SaverAnnotation // анотація
+    public void save() throws IOException { // метод для збереження тексту в документ
+        var fileForWriter = Container.class.getAnnotation(SaveToAnnotation.class).fileParam1(); // створюємо змінну = файл
+        // для збереження даних (як адресу використовуємо аргумент параметра анотації)
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileForWriter))) { //с помощью try with resource,
-            //который самостоятельно закрывает потоки, создаем поток для записи данных в буфер, а потом целым куском пишем в
-            //указанный файл
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileForWriter))) { // за допомогою try with resources,
+            // який самостійно закриває потоки, створюємо потік для запису даних у буфер, а потім цілим блоком пишемо в
+            // вказаний файл
             bufferedWriter.write(text);
         }
     }
 
     @Override
-    public String toString() { //переопределяем метод
+    public String toString() { // перевизначаємо метод
         return "Container{" +
                 "text='" + text + '\'' +
                 '}';
