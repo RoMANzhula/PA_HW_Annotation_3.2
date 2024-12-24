6c93253ee7dc2103d35ad91d8f62ab5697a9dc92
Roman Manzhula <roman1985al@ukr.net>
2024-12-24 21:48:27 +0200

    Update Main.java
    In Ukrainian

--- PA_Annotation_example_2_02.01.2023/src/Main.java
+++ PA_Annotation_example_2_02.01.2023/src/Main.java
@@ -2,8 +2,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        SaverClass saverClass = new SaverClass(); //создаем новый обьект типа и поведения класса SaverClass
-        saverClass.saveTextToFile(Container.class); //для нового обьект вызываем метод сохранитьТекстВФайл(аргумент: класс Контейнер)
-
+        SaverClass saverClass = new SaverClass(); //створюємо новий об'єкт типу та поведінки класу SaverClass
+        saverClass.saveTextToFile(Container.class); //для нового об'єкту викликаємо метод зберегтиТекстВФайл(аргумент: клас Контейнер)
     }
 }
