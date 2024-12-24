93b771bd6b590fa5cb11a0a685a18c1ab9858d25
Roman Manzhula <roman1985al@ukr.net>
2024-12-24 21:53:12 +0200

    Update SaverClass.java
    In Ukrainian

--- PA_Annotation_example_2_02.01.2023/src/SaverClass.java
+++ PA_Annotation_example_2_02.01.2023/src/SaverClass.java
@@ -3,14 +3,14 @@ import java.lang.reflect.Method;
 
 public class SaverClass {
     public void saveTextToFile(Class clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException {
-       //получаем методанные из класса Контейнер (относительно созданных в нем методов)
-        Method[] methods = clazz.getDeclaredMethods(); //получаем массив из методов класса Контейнер
-        for (Method method : methods) { //циклом "для каждого" проходимся по каждому методу из массива методов
-            if (method.isAnnotationPresent(SaverAnnotation.class)) { //если метод помечен аннотацией @SaverAnnotation, то
-                var obj = clazz.newInstance(); //создаем новый экземпляр, на котором будет вызываться метод invoke(obj) для
-                //нестатического метода нужен обьект в метод invoke()
-                method.invoke(obj); //передаем каждый метод на обьект
-                // System.out.println(method); //печатаем каждый задекларированный метод
+        // отримуємо методи з класу Контейнер (відносно створених в ньому методів)
+        Method[] methods = clazz.getDeclaredMethods(); // отримуємо масив методів класу Контейнер
+        for (Method method : methods) { // циклом "для кожного" проходимо по кожному методу з масиву методів
+            if (method.isAnnotationPresent(SaverAnnotation.class)) { // якщо метод помічений анотацією @SaverAnnotation, то
+                var obj = clazz.newInstance(); // створюємо новий екземпляр, на якому буде викликаний метод invoke(obj) для
+                // нестатичного методу потрібен об'єкт у методі invoke()
+                method.invoke(obj); // передаємо кожен метод на об'єкт
+                // System.out.println(method); // виводимо кожен задекларований метод
             }
         }
     }
